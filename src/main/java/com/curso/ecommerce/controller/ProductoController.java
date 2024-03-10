b3dd59f7489d36d8d262f2e5c2fd4232594351cf
rcrtt <renatocrotta308@gmail.com>
2024-03-10 09:17:27 -0300

    creacion de eliminacion de productos

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,13 +1,15 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.ProductoServicio;
@@ -40,6 +42,31 @@ public class ProductoController {
 		u=new Usuario(1,"","","","","","","");
 		producto.setUsuario(u);
 		productoServ.saveProduct(producto);
+		return "redirect:/productos";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer id, Model model) {
+		Producto producto=new Producto();
+		Optional<Producto> optionalProducto=productoServ.getProducto(id);
+		producto=optionalProducto.get();
+		LOGGER.info("Producto buscado: {}", producto);
+		model.addAttribute("p", producto);
+		
+		return "productos/edit";
+	}
+	
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		productoServ.saveProduct(producto);
+		return "redirect:/productos";
+		
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Integer id) {
+		
+		productoServ.deleteProduct(id);
 		return "redirect:/productos";
 	}
 }
